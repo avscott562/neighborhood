dfb2d152068dba9f7f2e3a7b14a760ac75c4c080
aliciascott <avscott@pacbell.net>
2019-04-13 12:59:36 -0700

    add additional markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,9 +6,13 @@ import { withGoogleMap, GoogleMap, withScriptjs, Marker } from "react-google-map
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={15}
-    defaultCenter={{ lat: 34.043018, lng: -118.267254 }}
+    defaultCenter={{ lat: 34.053018, lng: -118.267254 }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 34.043018, lng: -118.267254 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 34.045291, lng: -118.266712 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 34.04769, lng: -118.250558 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 34.042017, lng: -118.258748 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 34.056052, lng: -118.268867 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 34.060692, lng: -118.282764 }} />}
   </GoogleMap>
 ))
 
