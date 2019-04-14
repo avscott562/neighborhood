3515d05c2d435b9c405541e4102d83db89b50230
aliciascott <avscott@pacbell.net>
2019-04-13 19:51:16 -0700

    moved map creation back to app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import Map from './components/Map.js'
 import './App.css';
 
 class App extends Component {
 
+  initMap() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 34.053018, lng: -118.267254 },
+      zoom: 15
+    });
+  }
+
+  loadScript() {
+    let script = this.createScript();
+    let body = document.getElementById('body');
+    body.appendChild(script);
+    window.initMap = this.initMap;
+  }
+
+  createScript() {
+    let script = document.createElement('script');
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDbCcwSMuj4y-ECpL-lfHUhPvwzSoFhf24&libraries=geometry,drawing,places&callback=initMap";
+    script.async = true;
+    script.defer = true;
+    return script;
+  }
+
 
   render() {
+    this.loadScript();
     return (
       <main>
-        <div id="map">
-          <Map/>
-        </div>
+        <div id="map"></div>
       </main>
     );
   }
